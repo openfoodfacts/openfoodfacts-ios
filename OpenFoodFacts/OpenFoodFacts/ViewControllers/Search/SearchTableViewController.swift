4ea92b302a910b791f734867d8e893433d979295
tovkal <Tovkal@gmail.com>
2017-04-11 19:46:00 +0200

    Implement product search

--- OpenFoodFacts/OpenFoodFacts/ViewControllers/Search/SearchTableViewController.swift
+++ OpenFoodFacts/OpenFoodFacts/ViewControllers/Search/SearchTableViewController.swift
@@ -11,39 +11,32 @@ import UIKit
 class SearchTableViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var searchController: UISearchBar!
+    var searchController: UISearchController!
     
     var products = [Product(name: "Coca Cola Light", brand: "Coca Cola", quantity: "33 ml"),
+                    Product(name: "Coca Cola", brand: "Coca Cola", quantity: "33 ml"),
                     Product(name: "Fanta", brand: "Fanta", quantity: "33 ml"),
                     Product(name: "7UP", brand: "Coca Cola", quantity: "33 ml")]
+    var filteredProducts = [Product]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureTableView()
+        configureSearchController()
+    }
+    
+    fileprivate func configureTableView() {
         tableView.register(UINib(nibName: String(describing: ProductTableViewCell.self), bundle: nil), forCellReuseIdentifier: String(describing: ProductTableViewCell.self))
-        
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    fileprivate func configureSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
     }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
 
 extension SearchTableViewController: UITableViewDataSource {
@@ -53,59 +46,38 @@ extension SearchTableViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return filteredProducts.count
+        }
+        
         return products.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ProductTableViewCell.self), for: indexPath) as! ProductTableViewCell
         
-        cell.configure(withProduct: products[indexPath.row])
+        let product = searchController.isActive && searchController.searchBar.text != "" ? filteredProducts[indexPath.row] : products[indexPath.row]
+        cell.configure(withProduct: product)
         
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
-    
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
 }
 
 extension SearchTableViewController: UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension SearchTableViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let query = searchController.searchBar.text, !query.isEmpty {
+            filteredProducts = products.filter { product in return product.name.contains(query) }
+        } else {
+            
+        }
+        
+        tableView.reloadData()
+    }
 }
