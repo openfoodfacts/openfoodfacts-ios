8c170d4161d7382333e8a871a7ef344dfdfb48e9
tovkal <Tovkal@gmail.com>
2017-05-24 07:42:55 +0200

    Add Crashlytics for better crash reports

--- OpenFoodFacts/AppDelegate.swift
+++ OpenFoodFacts/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self])
         return true
     }
 
