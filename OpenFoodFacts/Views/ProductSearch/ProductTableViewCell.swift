5e925dc83cc5902f16a0a35261a42eaae7bcf739
tovkal <Tovkal@gmail.com>
2017-06-20 19:33:19 +0200

    Only show Brand and Quantity labels in search results when they have values

--- OpenFoodFacts/Views/ProductSearch/ProductTableViewCell.swift
+++ OpenFoodFacts/Views/ProductSearch/ProductTableViewCell.swift
@@ -13,21 +13,38 @@ class ProductTableViewCell: UITableViewCell {
     
     @IBOutlet weak var photo: UIImageView!
     @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var brand: UILabel!
-    @IBOutlet weak var quantity: UILabel!
+    @IBOutlet weak var brandLabel: UILabel!
+    @IBOutlet weak var separatorLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
     
     func configure(withProduct product: Product) {
         name.text = product.name
-        quantity.text = product.quantity
         
-        if let brands = product.brands, !brands.isEmpty {
-            brand.text = brands[0]
+        if let quantity = product.quantity, !quantity.isEmpty {
+            quantityLabel.text = quantity
+        } else {
+            quantityLabel.isHidden = true
+        }
+        
+        if let brand = product.brands?[0], !brand.isEmpty {
+            brandLabel.text = brand
+        } else {
+            brandLabel.isHidden = true
+        }
+        
+        if brandLabel.isHidden || quantityLabel.isHidden {
+            separatorLabel.isHidden = true
         }
         
         if let imageUrl = product.frontImageUrl ?? product.imageUrl, let url = URL(string: imageUrl) {
-            // TODO Placeholder image or loading
             photo.kf.indicatorType = .activity
             photo.kf.setImage(with: url)
         }
+    }
+    
+    override func prepareForReuse() {
+        quantityLabel.isHidden = false
+        brandLabel.isHidden = false
+        separatorLabel.isHidden = false
     }
 }
