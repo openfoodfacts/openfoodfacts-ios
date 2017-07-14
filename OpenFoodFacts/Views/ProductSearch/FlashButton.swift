dbb47e3912212f00e485d7d1172075409092b35d
tovkal <Tovkal@gmail.com>
2017-07-14 08:23:50 +0200

    Refactor FlashButton view

--- OpenFoodFacts/Views/ProductSearch/FlashButton.swift
+++ OpenFoodFacts/Views/ProductSearch/FlashButton.swift
@@ -16,6 +16,8 @@ enum FlashStatus {
 }
 
 @IBDesignable class FlashButton: UIView {
+    @IBInspectable fileprivate var flashButtonSize: CGFloat = 40
+    
     var state = FlashStatus.off {
         didSet {
             switch state {
@@ -27,45 +29,38 @@ enum FlashStatus {
         }
     }
     
-    fileprivate var rectangle = UIView()
     fileprivate var flashImageView = UIImageView()
     
     init() {
-        let frame = CGRect(x: 0, y: 0, width: 40, height: 40)
-        
-        rectangle.frame = frame
-        rectangle.layer.cornerRadius = rectangle.frame.size.width / 2
-        rectangle.layer.masksToBounds = true
-        rectangle.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        
-        let image = UIImage(named: flashOffImageName)! // Show always be an asset
-        
-        flashImageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        var rectangleConstraints = [NSLayoutConstraint]()
-        rectangleConstraints.append(NSLayoutConstraint(item: flashImageView, attribute: .top, relatedBy: .equal, toItem: rectangle, attribute: .top, multiplier: 1, constant: 8))
-        rectangleConstraints.append(NSLayoutConstraint(item: rectangle, attribute: .bottom, relatedBy: .equal, toItem: flashImageView, attribute: .bottom, multiplier: 1, constant: 7))
-        rectangleConstraints.append(NSLayoutConstraint(item: flashImageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: image.size.width))
-        rectangleConstraints.append(NSLayoutConstraint(item: flashImageView, attribute: .centerX, relatedBy: .equal, toItem: rectangle, attribute: .centerX, multiplier: 1, constant: 0))
-        
-        rectangle.addSubview(flashImageView)
-        rectangle.addConstraints(rectangleConstraints)
-        
-        // Init
-        super.init(frame: frame)
-        
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(rectangle)
-        
-        var sizeConstraints = [NSLayoutConstraint]()
-        sizeConstraints.append(NSLayoutConstraint(item: self, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40))
-        sizeConstraints.append(NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40))
-        
-        self.addConstraints(sizeConstraints)
+        super.init(frame: .zero)
+        configureView()
+        configureFlashImageView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func configureView() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.layer.cornerRadius = flashButtonSize / 2
+        self.layer.masksToBounds = true
+        self.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        self.addSubview(flashImageView)
+        
+        self.addConstraint(NSLayoutConstraint(item: self, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: flashButtonSize))
+        self.addConstraint(NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: flashButtonSize))
+    }
+    
+    fileprivate func configureFlashImageView() {
+        flashImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let image = UIImage(named: flashOffImageName)! // Show always be an asset
+        
+        self.addConstraint(NSLayoutConstraint(item: flashImageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 8))
+        self.addConstraint(NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: flashImageView, attribute: .bottom, multiplier: 1, constant: 7))
+        self.addConstraint(NSLayoutConstraint(item: flashImageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: image.size.width))
+        self.addConstraint(NSLayoutConstraint(item: flashImageView, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
     }
     
     override func willMove(toSuperview newSuperview: UIView?) {
