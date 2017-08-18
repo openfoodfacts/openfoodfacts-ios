1c0d633072d90898797cdbd22055bf91897ca444
tovkal <Tovkal@gmail.com>
2017-08-18 22:40:31 +0200

    SearchTableViewController Tests

--- OpenFoodFactsTests/ProductServiceMock.swift
+++ OpenFoodFactsTests/ProductServiceMock.swift
@@ -8,14 +8,26 @@
 
 import Foundation
 @testable import OpenFoodFacts
+import ObjectMapper
 
 class ProductServiceMock: ProductApi {
-    func getProducts(for query: String, page: Int, onSuccess: @escaping (ProductsResponse) -> Void, onError: @escaping (Error) -> Void) {
+    var query: String?
+    var page: Int?
+    var productResponse: ProductsResponse!
+    let error = NSError(domain:NSURLErrorDomain, code:-1009, userInfo: nil)
 
+    func getProducts(for query: String, page: Int, onSuccess: @escaping (ProductsResponse) -> Void, onError: @escaping (Error) -> Void) {
+        self.query = query
+        self.page = page
+
+        if "Fanta" == query {
+            onSuccess(productResponse)
+        } else {
+            onError(error)
+        }
     }
 
     func getProduct(byBarcode barcode: String, onSuccess: @escaping (ProductsResponse) -> Void, onError: @escaping (Error) -> Void) {
-
     }
 
     func postImage(_ productImage: ProductImage, barcode: String, onSuccess: @escaping () -> Void, onError: @escaping (Error) -> Void) {
--- OpenFoodFactsTests/ViewControllers/Products/Search/SearchTableViewController/SearchTableViewControllerTests.swift
+++ OpenFoodFactsTests/ViewControllers/Products/Search/SearchTableViewController/SearchTableViewControllerTests.swift
@@ -9,11 +9,12 @@
 import XCTest
 @testable import OpenFoodFacts
 import Nimble
+import ObjectMapper
 
 class SearchTableViewControllerTests: XCTestCase {
 
     var viewController: SearchTableViewController!
-    lazy var productApi = ProductServiceMock()
+    var productApi: ProductServiceMock!
 
     override func setUp() {
         super.setUp()
@@ -22,24 +23,139 @@ class SearchTableViewControllerTests: XCTestCase {
         let tabBarController = storyboard.instantiateInitialViewController() as! UITabBarController // swiftlint:disable:this force_cast
         let navigationController = tabBarController.viewControllers?[0] as! UINavigationController // swiftlint:disable:this force_cast
         viewController = navigationController.topViewController as! SearchTableViewController // swiftlint:disable:this force_cast
+        productApi = ProductServiceMock()
+        productApi.productResponse = ProductsResponse(map: Map(mappingType: .fromJSON, JSON: [String: Any]()))!
+
         viewController.productApi = productApi
 
         UIApplication.shared.keyWindow!.rootViewController = viewController
 
-        XCTAssertNotNil(navigationController.view)
-        XCTAssertNotNil(viewController.view)
+        expect(navigationController.view).notTo(beNil())
+        expect(self.viewController.view).notTo(beNil())
     }
 
     func testInitialState() {
-        switch viewController.state {
-        case .initial:
-            break
-        default:
-            XCTFail("Expected initial state")
-        }
+        expect(self.viewController.state).to(beInitial())
     }
 
     func testInitialBackgroundView() {
-        XCTAssertEqual(viewController.tableView.backgroundView, viewController.initialView)
+        expect(self.viewController.tableView.backgroundView).to(equal(viewController.initialView))
+    }
+
+    // MARK: - UISearchResultsUpdating
+
+    func testSearchInputGeneratesQueryForFirstPages() {
+        let query = "Fanta"
+        let searchBar = viewController.searchController.searchBar
+        searchBar.text = query
+        viewController.searchBar(searchBar, textDidChange: query)
+        viewController.updateSearchResults(for: viewController.searchController)
+
+        expect(self.productApi.query).toEventually(equal(query))
+        expect(self.productApi.page).toEventually(equal(1))
+    }
+
+    // MARK: - UISearchBarDelegate
+
+    func testClearingSearchBarReturnsToInitialState() {
+        let query = "Fanta"
+        let searchBar = viewController.searchController.searchBar
+        searchBar.text = query
+        viewController.state = .content(ProductsResponse(map: Map(mappingType: .fromJSON, JSON: [String: Any]()))!)
+        viewController.searchBar(searchBar, textDidChange: "")
+
+        expect(self.viewController.state).to(beInitial())
+    }
+
+    func testTappingSearchBarCancelButtonReturnsToInitialState() {
+        let query = "Fanta"
+        let searchBar = viewController.searchController.searchBar
+        searchBar.text = query
+        viewController.state = .content(ProductsResponse(map: Map(mappingType: .fromJSON, JSON: [String: Any]()))!)
+        viewController.searchBarCancelButtonClicked(searchBar)
+
+        expect(self.viewController.state).to(beInitial())
+    }
+
+    func testEditingSearchBarShowsCancelButton() {
+        let searchBar = viewController.searchController.searchBar
+        expect(searchBar.showsCancelButton).to(beFalse())
+        viewController.searchBarTextDidBeginEditing(searchBar)
+
+        expect(self.viewController.searchController.searchBar.showsCancelButton).to(beTrue())
+    }
+
+    func testFinishEditingSearchBarHidesCancelButton() {
+        let searchBar = viewController.searchController.searchBar
+        searchBar.showsCancelButton = true
+        viewController.searchBarTextDidEndEditing(searchBar)
+
+        expect(self.viewController.searchController.searchBar.showsCancelButton).to(beFalse())
+    }
+
+    // MARK: - Data source
+
+    func testGetProductsChangesStateToContentWhenGotNewResponse() {
+        let page = 1
+        let query = "Fanta"
+        let bundle = Bundle(for: type(of: self))
+        guard let url = bundle.url(forResource: "GetProductsByNameSuccess", withExtension: "json") else { XCTFail("Failed to get json file"); return }
+        guard let data = try? Data(contentsOf: url) else { XCTFail("Failed to read json file"); return }
+        guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []) else { XCTFail("Failed to parse json"); return }
+        guard let json = jsonObject as? [String: Any] else { XCTFail("Failed to cast json"); return }
+        let map = Map(mappingType: .fromJSON, JSON: json)
+        let productResponse = ProductsResponse(map: map)!
+        productResponse.mapping(map: map)
+        productApi.productResponse = productResponse
+        viewController.getProducts(page: page, withQuery: query)
+
+        expect(self.viewController.state).to(beContent { response in
+            expect(response) === productResponse
+        })
+    }
+
+    func testGetProductsChangesStateToEmptyWhenResponseIsEmpty() {
+        let page = 1
+        let query = "Fanta"
+        viewController.getProducts(page: page, withQuery: query)
+
+        expect(self.viewController.state).to(beEmpty())
+    }
+
+    func testGetProductsAppendsResponseWhenStatusAlreadyIsContent() {
+        // TODO
+    }
+
+    func testGetProductsChangesStateToErrorWhenApiCallFails() {
+        // TODO
+    }
+
+    private func beInitial() -> Predicate<SearchTableViewController.State> {
+        return Predicate.define("be Initial") { expression, message in
+            if let actual = try expression.evaluate(), case .initial = actual {
+                return PredicateResult(status: .matches, message: message)
+            }
+            return PredicateResult(status: .fail, message: message)
+        }
+    }
+
+    private func beEmpty() -> Predicate<SearchTableViewController.State> {
+        return Predicate.define("be Empty") { expression, message in
+            if let actual = try expression.evaluate(), case .empty = actual {
+                return PredicateResult(status: .matches, message: message)
+            }
+            return PredicateResult(status: .fail, message: message)
+        }
+    }
+
+    private func beContent(test: @escaping (ProductsResponse) -> Void = { _ in }) -> Predicate<SearchTableViewController.State> {
+        return Predicate.define("be Content") { expression, message in
+            if let actual = try expression.evaluate(),
+                case let .content(productResponse) = actual {
+                test(productResponse)
+                return PredicateResult(status: .matches, message: message)
+            }
+            return PredicateResult(status: .fail, message: message)
+        }
     }
 }
