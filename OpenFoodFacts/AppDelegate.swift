43131babd14987e21b05248d9bdf822d24a45771
tovkal <Tovkal@gmail.com>
2017-08-28 21:35:11 +0200

    Add scan home screen quick action

--- OpenFoodFacts/AppDelegate.swift
+++ OpenFoodFacts/AppDelegate.swift
@@ -25,16 +25,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
         let productApi = ProductService()
-
-        if let tab = window?.rootViewController as? UITabBarController {
-            for child in tab.viewControllers ?? [] {
-                if let navController = child as? UINavigationController, let vc = navController.topViewController as? SearchTableViewController {
-                    vc.productApi = productApi
-                }
-            }
-        }
+        let searchTVC = findSearchTVCFromRootController()
+        searchTVC?.productApi = productApi
 
         return true
+    }
+
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        if shortcutItem.type == "scan" {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let rootVC = storyboard.instantiateInitialViewController()
+            window?.rootViewController = rootVC
+
+            let productApi = ProductService()
+            let searchTVC = findSearchTVCFromRootController()
+            searchTVC?.productApi = productApi
+
+            let scanVC = ScannerViewController(productApi: productApi)
+            searchTVC?.navigationController?.pushViewController(scanVC, animated: false)
+        }
     }
 
     fileprivate func configureLog() {
@@ -49,5 +58,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         systemDestination.showDate = true
         log.add(destination: systemDestination)
         log.logAppDetails()
+
+    }
+
+    fileprivate func findSearchTVCFromRootController() -> SearchTableViewController? {
+        if let tab = window?.rootViewController as? UITabBarController {
+            for child in tab.viewControllers ?? [] {
+                if let navController = child as? UINavigationController, let vc = navController.topViewController as? SearchTableViewController {
+                    return vc
+                }
+            }
+        }
+
+        return nil
     }
 }
