280b6c145a3b60190b4eceab427839bef603e23b
tovkal <Tovkal@gmail.com>
2017-09-11 22:26:16 +0200

    Test String extension

---
+++ OpenFoodFactsTests/Extensions/StringTests.swift
@@ -0,0 +1,20 @@
+//
+//  StringTests.swift
+//  OpenFoodFacts
+//
+//  Created by Andrés Pizá Bückmann on 11/09/2017.
+//  Copyright © 2017 Andrés Pizá Bückmann. All rights reserved.
+//
+
+import XCTest
+@testable import OpenFoodFacts
+
+class StringTests: XCTestCase {
+    func testIsNumberReturnsTrueWhenStringIsNumber() {
+        XCTAssertTrue("2".isNumber())
+    }
+
+    func testIsNumberReturnsFalseWhenStringIsNotNumber() {
+        XCTAssertFalse("a".isNumber())
+    }
+}
