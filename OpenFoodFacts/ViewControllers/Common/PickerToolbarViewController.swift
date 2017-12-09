31edfffbbb4c68b46dbe377cc0ec982acaf321b6
Andrés Pizá <Tovkal@gmail.com>
2017-12-09 15:45:32 +0100

    Make PickerView toolbar title optional

--- OpenFoodFacts/ViewControllers/Common/PickerToolbarViewController.swift
+++ OpenFoodFacts/ViewControllers/Common/PickerToolbarViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class PickerToolbarViewController: UIViewController {
-    private let toolbarTitle: String
+    private let toolbarTitle: String?
     private weak var delegate: PickerViewDelegate?
 
-    init(title: String, delegate: PickerViewDelegate? = nil) {
+    init(title: String?, delegate: PickerViewDelegate? = nil) {
         self.toolbarTitle = title
         self.delegate = delegate
         super.init(nibName: nil, bundle: nil)
@@ -23,21 +23,29 @@ class PickerToolbarViewController: UIViewController {
     }
 
     override func loadView() {
+        var items = [UIBarButtonItem]()
+
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+        if self.toolbarTitle != nil {
+            let titleLabel = UILabel()
+            titleLabel.text = toolbarTitle
+            titleLabel.textColor = UIColor(red: 0.78, green: 0.78, blue: 0.80, alpha: 1.0)
+            let title = UIBarButtonItem(customView: titleLabel)
+
+            items.append(contentsOf: [space, title])
+        }
+
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self,
                                          action: #selector(PickerToolbarViewController.dismissPicker))
 
-        let titleLabel = UILabel()
-        titleLabel.text = toolbarTitle
-        titleLabel.textColor = UIColor(red: 0.78, green: 0.78, blue: 0.80, alpha: 1.0)
-        let title = UIBarButtonItem(customView: titleLabel)
-
-        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        items.append(contentsOf: [space, doneButton])
 
         let toolbar = UIToolbar()
         toolbar.sizeToFit()
         toolbar.translatesAutoresizingMaskIntoConstraints = false
         toolbar.isUserInteractionEnabled = true
-        toolbar.setItems([space, title, space, doneButton], animated: false)
+        toolbar.setItems(items, animated: true)
 
         self.view = toolbar
     }
