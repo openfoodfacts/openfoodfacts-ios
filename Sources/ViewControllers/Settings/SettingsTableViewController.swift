b8cb1b6fb060056b35880e0aaff5ec318781b04d
Rudrank Riyam <rudrankriyam@gmail.com>
2019-02-28 03:46:39 +0530

    Resolves #149
    Added an alert when the iPhone doesn't have Mail App configured

--- Sources/ViewControllers/Settings/SettingsTableViewController.swift
+++ Sources/ViewControllers/Settings/SettingsTableViewController.swift
@@ -18,28 +18,21 @@ enum SettingsSection: Int {
 
 class SettingsTableViewController: UITableViewController, MFMailComposeViewControllerDelegate, DataManagerClient {
     @IBOutlet weak var scanOnLaunchSwitch: UISwitch!
-
+    
     var dataManager: DataManagerProtocol!
-
+    
     private let frequentlyAskedQuestionsIndexPath = IndexPath(row: 0, section: 1)
     private let allergensAlertsIndexPath = IndexPath(row: 1, section: 0)
-
     private let discoverIndexPath = IndexPath(row: 1, section: 1)
-
     private let howToContributeIndexPath = IndexPath(row: 0, section: 2)
     private let supportOpenFoodFactsIndexPath = IndexPath(row: 1, section: 2)
     private let translateOpenFoodFactsIndexPath = IndexPath(row: 2, section: 2)
-
     private let contactTheTeamIndexPath = IndexPath(row: 1, section: 3)
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.title = "settings.tab-bar.item".localized
-
         scanOnLaunchSwitch.isOn = UserDefaults.standard.bool(forKey: UserDefaultsConstants.scanningOnLaunch)
     }
-
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         guard let settings = SettingsSection(rawValue: section) else { return "" }
         switch settings {
@@ -53,12 +46,10 @@ class SettingsTableViewController: UITableViewController, MFMailComposeViewContr
             return "settings.sections.about".localized
         }
     }
-
     @IBAction func didTapOpenBeautyFacts(_ sender: UIButton) {
         guard let url = URL(string: URLs.OpenBeautyFacts) else { return }
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
-
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         var url: URL?
         var urlsupport: URL?
@@ -80,46 +71,43 @@ class SettingsTableViewController: UITableViewController, MFMailComposeViewContr
         default:
             break
         }
-
         if let url = url {
             openUrlInApp(url)
         } else if let url = urlsupport {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
-
     @IBAction func didSwitchScanOnLaunch(_ sender: UISwitch) {
         UserDefaults.standard.set(sender.isOn, forKey: UserDefaultsConstants.scanningOnLaunch)
     }
-
-    func contactTheTeam() {
-        if !MFMailComposeViewController.canSendMail() {
-            print("Mail services are not available")
-            return
-        }
-        sendEmail()
-    }
-
     fileprivate func openAllergensAlerts() {
         let alertsVC = AllergensAlertsTableViewController()
         alertsVC.dataManager = dataManager
         self.navigationController?.pushViewController(alertsVC, animated: true)
     }
-
-    func sendEmail() {
-        let composeVC = MFMailComposeViewController()
-        composeVC.mailComposeDelegate = self
-
-        composeVC.setToRecipients(["contact@openfoodfacts.org"])
-        composeVC.setSubject("Open Food Facts: ")
-        composeVC.setMessageBody("Thank you for contacting us.", isHTML: false)
-
-        self.present(composeVC, animated: true, completion: nil)
+    func contactTheTeam() {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
     }
-
-    func mailComposeController(_ controller: MFMailComposeViewController,
-                               didFinishWith result: MFMailComposeResult, error: Error?) {
-
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerViewController = MFMailComposeViewController()
+        mailComposerViewController.mailComposeDelegate = self
+        mailComposerViewController.setToRecipients(["contact@openfoodfacts.org"])
+        mailComposerViewController.setSubject("Open Food Facts: ")
+        mailComposerViewController.setMessageBody("settings.contact.message".localized, isHTML: false)
+        return mailComposerViewController
+    }
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertController(title: "settings.contact-alert.title".localized, message: "settings.contact-alert.message".localized, preferredStyle: .alert)
+        let yesAction = UIAlertAction(title: "alert.action.ok".localized, style: .default)
+        sendMailErrorAlert.addAction(yesAction)
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
+    }
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true, completion: nil)
     }
 }
