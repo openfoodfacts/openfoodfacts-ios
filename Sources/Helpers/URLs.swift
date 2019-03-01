f00d113c8b2c442b51c4c267638345454661496d
Rudrank Riyam <rudrankriyam@gmail.com>
2019-03-01 16:05:44 +0530

    Resolves #199
    The URLs are now localised

--- Sources/Helpers/URLs.swift
+++ Sources/Helpers/URLs.swift
@@ -9,21 +9,24 @@
 import Foundation
 
 struct URLs {
-    static let Discover = "https://world.openfoodfacts.org/discover"
-    static let HowToContribute = "https://world.openfoodfacts.org/contribute"
-    static let CreateAccount = "https://world.openfoodfacts.org/cgi/user.pl"
-    static let ForgotPassword = "https://world.openfoodfacts.org/cgi/reset_password.pl"
-    static let Edit = "https://world.openfoodfacts.org/cgi/product.pl?type=edit&code="
-    static let YourContributions = "https://world.openfoodfacts.org/contributor/"
-    static let ProductBaseURLWithLanguagePlaceholder = "https://world-LANGUAGE.openfoodfacts.org/product/"
+    static let codeLang = Bundle.main.preferredLocalizations.first ?? "en"
+    static let baseUrl = "https://world-\(codeLang).openfoodfacts.org"
+    
+    static let Discover = "\(baseUrl)/discover"
+    static let HowToContribute = "\(baseUrl)/contribute"
+    static let CreateAccount = "\(baseUrl)/cgi/user.pl"
+    static let ForgotPassword = "\(baseUrl)/cgi/reset_password.pl"
+    static let Edit = "\(baseUrl)/cgi/product.pl?type=edit&code="
+    static let YourContributions = "\(baseUrl)/contributor/"
+    static let ProductBaseURLWithLanguagePlaceholder = "\(baseUrl)/product/"
     static let OpenBeautyFacts = "itms-apps://itunes.apple.com/us/app/open-beauty-facts/id1122926380?mt=8"
     static let SupportOpenFoodFacts = "https://www.helloasso.com/associations/open-food-facts/formulaires/1/widget/en"
     static let TranslateOpenFoodFacts = "https://crowdin.com/project/openfoodfacts"
-    static let FrequentlyAskedQuestions = "https://world.openfoodfacts.org/faq"
+    static let FrequentlyAskedQuestions = "\(baseUrl)/faq"
     static let ProductBaseURL: String = {
         return ProductBaseURLWithLanguagePlaceholder.replacingOccurrences(of: "LANGUAGE", with: Locale.current.languageCode!)
     }()
-
+    
     static func urlForProduct(with code: String?) -> String {
         guard let code = code else {
             return Discover
