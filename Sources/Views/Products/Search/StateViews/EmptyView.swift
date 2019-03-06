9a77fab0aae09c6fed9ee3820aaf6e876cfa71cd
Rudrank Riyam <rudrankriyam@gmail.com>
2019-03-06 22:19:25 +0530

    Update EmptyView.swift (#256)

--- Sources/Views/Products/Search/StateViews/EmptyView.swift
+++ Sources/Views/Products/Search/StateViews/EmptyView.swift
@@ -17,8 +17,9 @@ class EmptyView: StateView {
         let emptyLabel = UILabel()
         emptyLabel.translatesAutoresizingMaskIntoConstraints = false
         emptyLabel.text = "product-search.no-results".localized
+        emptyLabel.numberOfLines = 0
         emptyLabel.textAlignment = .center
-        emptyLabel.sizeToFit()
+        emptyLabel.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.size.width * 0.9).isActive = true
 
         let centerHorizontally = NSLayoutConstraint(item: emptyLabel, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0)
         let centerVertically = NSLayoutConstraint(item: emptyLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)
