50631ab2a2b5e4985f868ae45a6cc1d182f89c4a
Timothee MATO <ext-tmato@divalto.com>
2019-10-14 13:53:14 +0200

    Add base files for IngredientsAnalysisTableViewCell

---
+++ Sources/Views/Products/Detail/Summary/IngredientsAnalysisTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  IngredientsAnalysisTableViewCell.swift
+//  OpenFoodFacts
+//
+//  Created by Timothee MATO on 14/10/2019.
+//  Copyright © 2019 Andrés Pizá Bückmann. All rights reserved.
+//
+
+import UIKit
+
+class IngredientsAnalysisTableViewCell: ProductDetailBaseCell {
+        
+    override func configure(with formRow: FormRow, in viewController: FormTableViewController) {
+        
+    }
+}
