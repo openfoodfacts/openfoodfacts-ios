2598d10ce1b166156c3b0b9c66b536ea33f285ef
Philippe Auriach <p.auriach@gmail.com>
2020-01-28 11:00:04 +0100

    fix #559

--- Sources/Views/Products/Detail/Summary/IngredientsAnalysisTableViewCell.swift
+++ Sources/Views/Products/Detail/Summary/IngredientsAnalysisTableViewCell.swift
@@ -21,6 +21,13 @@ class IngredientsAnalysisTableViewCell: ProductDetailBaseCell {
 
         self.viewController = viewController
 
+        stackView.arrangedSubviews.forEach {
+            if let view = $0 as? IngredientsAnalysisView {
+                view.removeGestureRecognizer()
+            }
+        }
+        stackView.removeAllViews()
+
         for detail in ingredientsAnalysisDetails {
             let analysisView = IngredientsAnalysisView.loadFromNib()
             analysisView.configure(detail: detail, missingIngredients: product.states?.contains("en:ingredients-to-be-completed") == true, ingredientsList: product.ingredientsListAnalysis)
