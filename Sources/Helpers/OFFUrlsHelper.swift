ff71720453bac2d26afc89b9b8615ab269bae955
Arnaud Leene <aleene@gmail.com>
2020-02-26 12:02:22 +0100

    Removed leftovers

--- Sources/Helpers/OFFUrlsHelper.swift
+++ Sources/Helpers/OFFUrlsHelper.swift
@@ -20,9 +20,6 @@ class OFFUrlsHelper: NSObject {
     static func url(for country: Country) -> URL {
         return URL(string: "\(baseUrl)/country/\(country.code)")!
     }
-    static func urlCountry(for string: String) -> URL {
-        return URL(string: "\(baseUrl)/country/\(string)")!
-    }
 
     static func url(forAdditive additive: Additive) -> URL {
         return URL(string: "\(baseUrl)/additive/\(additive.code)")!
@@ -30,10 +27,6 @@ class OFFUrlsHelper: NSObject {
 
     static func url(forAllergen allergen: Allergen) -> URL {
         return URL(string: "\(baseUrl)/allergen/\(allergen.code)")!
-    }
-
-    static func urlAllergen(for string: String) -> URL {
-        return URL(string: "\(baseUrl)/allergen/\(string)")!
     }
 
     static func url(forMineral mineral: Mineral) -> URL {
