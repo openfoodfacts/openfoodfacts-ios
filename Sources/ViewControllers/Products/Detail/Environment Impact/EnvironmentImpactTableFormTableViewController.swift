e9bca24dd8a5f6866e6da2d08a83f1f8c3da51cb
Arnaud Leene <aleene@gmail.com>
2020-03-08 16:52:10 +0100

    Iniital changes

--- Sources/ViewControllers/Products/Detail/Environment Impact/EnvironmentImpactTableFormTableViewController.swift
+++ Sources/ViewControllers/Products/Detail/Environment Impact/EnvironmentImpactTableFormTableViewController.swift
@@ -8,10 +8,30 @@
 
 import UIKit
 import XLPagerTabStrip
+import WebKit
 
 class EnvironmentImpactTableFormTableViewController: UIViewController {
 
-    fileprivate let webView: UIWebView = UIWebView()
+    fileprivate var webView: WKWebView? = nil {
+        didSet {
+            if #available(iOS 13.0, *) {
+                webView?.backgroundColor = UIColor.systemBackground
+            } else {
+                webView?.backgroundColor = UIColor.white
+            }
+            webView?.translatesAutoresizingMaskIntoConstraints = false
+            webView?.navigationDelegate = self
+            if let validWebView = self.webView {
+                self.view.addSubview(validWebView)
+                self.view.addConstraints([
+                    NSLayoutConstraint(item: validWebView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 8),
+                    NSLayoutConstraint(item: validWebView, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -8),
+                    NSLayoutConstraint(item: validWebView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 8),
+                    NSLayoutConstraint(item: validWebView, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: -8)
+                    ])
+            }
+        }
+    }
     var product: Product? {
         didSet {
             if isViewLoaded {
@@ -23,22 +43,7 @@ class EnvironmentImpactTableFormTableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if #available(iOS 13.0, *) {
-            webView.backgroundColor = UIColor.systemBackground
-        } else {
-            webView.backgroundColor = UIColor.white
-        }
-        webView.translatesAutoresizingMaskIntoConstraints = false
-        webView.delegate = self
-
-        self.view.addSubview(webView)
-
-        self.view.addConstraints([
-            NSLayoutConstraint(item: webView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 8),
-            NSLayoutConstraint(item: webView, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -8),
-            NSLayoutConstraint(item: webView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 8),
-            NSLayoutConstraint(item: webView, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: -8)
-            ])
+        self.webView = WKWebView()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -49,10 +54,10 @@ class EnvironmentImpactTableFormTableViewController: UIViewController {
 
     func reloadHTML() {
         guard let html = product?.environmentInfoCard else {
-            webView.isHidden = true
+            webView?.isHidden = true
             return
         }
-        webView.loadHTMLString(html, baseURL: nil)
+        webView?.loadHTMLString(html, baseURL: nil)
     }
 
 }
@@ -65,8 +70,10 @@ extension EnvironmentImpactTableFormTableViewController: IndicatorInfoProvider {
 
 }
 
-extension EnvironmentImpactTableFormTableViewController: UIWebViewDelegate {
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        webView.stringByEvaluatingJavaScript(from: "document.getElementsByTagName('body')[0].style.fontFamily =\"-apple-system-body\"")
+extension EnvironmentImpactTableFormTableViewController: WKNavigationDelegate {
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            webView.evaluateJavaScript("document.getElementsByTagName('body')[0].style.fontFamily =\"-apple-system-body\"", completionHandler: nil)
     }
+
 }
--- Sources/Views/Products/Detail/ProductDetailWebViewTableViewCell.swift
+++ Sources/Views/Products/Detail/ProductDetailWebViewTableViewCell.swift
@@ -7,43 +7,56 @@
 //
 
 import UIKit
+import WebKit
 
 class ProductDetailWebViewTableViewCell: ProductDetailBaseCell {
     override class var estimatedHeight: CGFloat { return 160 }
 
-    @IBOutlet weak var webView: UIWebView!
-    @IBOutlet weak var webViewHeightConstraint: NSLayoutConstraint!
+    var webView: WKWebView? = nil {
+        didSet {
+            webView?.scrollView.bounces = false
+            webView?.scrollView.isScrollEnabled = false
+            //webView?.navigationDelegate = self
+        }
+    }
+
+    var webViewHeightConstraint: NSLayoutConstraint?
 
     weak var tableView: UITableView?
 
+    private struct Constant {
+        static let Margin: CGFloat = 8.0
+    }
+    @IBOutlet weak var containerView: UIView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-
-        webView.scrollView.bounces = false
-        webView.scrollView.isScrollEnabled = false
-        webView.delegate = self
+        webView = WKWebView(frame: self.containerView.frame)
+        if webView != nil {
+            self.containerView?.addSubview(webView!)
+        }
     }
 
     override func configure(with formRow: FormRow, in viewController: FormTableViewController) {
         if let html = formRow.getValueAsString() {
             let font = UIFont.preferredFont(forTextStyle: .body)
             if #available(iOS 13.0, *) {
-                webView.loadHTMLString(html.htmlFormattedString(font: font, color: .label), baseURL: nil)
+                webView?.loadHTMLString(html.htmlFormattedString(font: font, color: .label), baseURL: nil)
             } else {
-                webView.loadHTMLString(html.htmlFormattedString(font: font, color: .black), baseURL: nil)
+                webView?.loadHTMLString(html.htmlFormattedString(font: font, color: .black), baseURL: nil)
             }
-            webView.isHidden = false
+            webView?.isHidden = false
         } else {
-            webView.isHidden = true
+            webView?.isHidden = true
         }
         self.tableView = viewController.tableView
     }
 }
 
-// MARK: UIWebViewDelegate
-extension ProductDetailWebViewTableViewCell: UIWebViewDelegate {
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        webViewHeightConstraint.constant = webView.scrollView.contentSize.height
+// MARK: WKNavigationDelegate
+extension ProductDetailWebViewTableViewCell: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webViewHeightConstraint?.constant = webView.scrollView.contentSize.height
         //webView.stringByEvaluatingJavaScript(from: "document.getElementsByTagName('body')[0].style.fontFamily =\"-apple-system-body\"")
 
         // to force redraw of cell height
@@ -51,6 +64,7 @@ extension ProductDetailWebViewTableViewCell: UIWebViewDelegate {
         tableView?.endUpdates()
     }
 }
+
 
 extension String {
 
@@ -73,7 +87,8 @@ func htmlFormattedString( font: UIFont, color: UIColor) -> String {
         }
     }
     // The table contains returns /n in strange places. Just to be sure they are removed.
-    return String(format: "<html><head><style>table {font-family: -apple-system; font-size: %@; color:#%@;}</style></head><body>%@</body></html>", String(describing: font.pointSize), colorHexString(color: color), self.replacingOccurrences(of: "\n", with: ""))
+    let string = "<html><head><style>table {font-family: -apple-system; font-size: %@; color:#%@;}</style></head><body>%@</body></html>"
+    return String(format: string, String(describing: font.pointSize), colorHexString(color: color), self.replacingOccurrences(of: "\n", with: ""))
     }
 
 }
