7ffda05c0f5886fa9683c47d8ffaa71eedcda827
Mohammed Abdullatif <mabdullatif@spotify.com>
2020-06-08 01:42:43 +0200

    Add documentation for "TaxonomiesParserProtocol".

--- Sources/Protocols/TaxonomiesParserProtocol.swift
+++ Sources/Protocols/TaxonomiesParserProtocol.swift
@@ -1,5 +1,11 @@
 import Foundation
 
+/**
+ The `TaxonomiesParserProtocol` A protocol used to describe the behaviours expected from 'TaxonomiesParser'.
+ By using a protocol, we can easily use dependency injection when we create an instance of 'TaxonomiesService'. This can make testing 'TaxonomiesService'
+ easier as we will be able to create a 'TaxonomiesParser' mock object that will conform to 'TaxonomiesParserProtocol' and will be injected to 'TaxonomiesService'
+ instance when we write the unit tests. The mock object of 'TaxonomiesParser' will contain simpler implementation that can make unit tests easier and faster.
+*/
 protocol TaxonomiesParserProtocol {
     func parseCategories(data: [String: Any]) -> [Category]
     func parseCountries(data: [String: Any]) -> [Country]
