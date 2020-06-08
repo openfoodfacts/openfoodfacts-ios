e2d83db34fc8cced5ca754e4f5aee92e625dd19a
Mohammed Abdullatif <mabdullatif@spotify.com>
2020-06-08 18:29:50 +0200

    Format comment text.

--- Sources/Helpers/NotificationCenterToken.swift
+++ Sources/Helpers/NotificationCenterToken.swift
@@ -3,7 +3,10 @@ import Foundation
 /**
  The `NotificationCenterToken` class Wraps the observer token received from NotificationCenter.addObserver(forName:object:queue:using:)
  and unregisters it in deinit.
- This binds the lifetime of the notification observation to the lifetime of the wrapper object. All we have to do is store the wrapper in a private property so that it gets destroyed when its owner gets deallocated. So this is equivalent to manual unregistering in deinit, but has the benefit that you can’t forget it anymore. And by making the property an Optional​<Notification​Token>, you can unregister anytime simply by assigning nil. This pattern is known as Resource acquisition is initialization (RAII).
+ This binds the lifetime of the notification observation to the lifetime of the wrapper object. All we have to do is store the wrapper in a private property so that it gets
+ destroyed when its owner gets deallocated. So this is equivalent to manual unregistering in deinit, but has the benefit that you can’t forget it anymore. And by making
+ the property an Optional​<Notification​Token>, you can unregister anytime simply by assigning nil.
+ This pattern is known as Resource acquisition is initialization (RAII).
 */
 final class NotificationCenterToken: NSObject {
     let notificationCenter: NotificationCenter
