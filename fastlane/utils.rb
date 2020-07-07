6ab2efe1e8774831f5eecd4437261e106d2a9da5
Philippe Auriach <p.auriach@gmail.com>
2020-07-07 18:09:31 +0200

    Use AppStoreConnectApi to connect to the appstore programatically

--- fastlane/utils.rb
+++ fastlane/utils.rb
@@ -3,17 +3,23 @@ require 'versionomy'
 
 def highest_app_version_number()
     puts "login into spaceship"
-    Spaceship::Tunes.login()
-    app = Spaceship::Tunes::Application.find(ENV['APP_BUNDLE_IDENTIFIER'])
+
+    token = Spaceship::ConnectAPI::Token.create
+    Spaceship::ConnectAPI.token = token
+
+    app = Spaceship::ConnectAPI.get_apps(filter: { bundleId: ENV['APP_BUNDLE_IDENTIFIER'] }).first
     
+    liveAppStoreVersion = app.get_live_app_store_version()
+    editAppStoreVersion = app.get_edit_app_store_version()
+
     liveVersion = nil
 
-    if app && app.live_version
-        liveVersion = app.live_version.version
+    if liveAppStoreVersion && liveAppStoreVersion.version_string
+        liveVersion = editAppStoreVersion.version_string
     end
 
-    if app && app.edit_version && liveVersion.nil?
-        editVersion = app.edit_version.version
+    if editAppStoreVersion && editAppStoreVersion.version_string && liveVersion.nil?
+        editVersion = editAppStoreVersion.version_string
         return liveVersion, editVersion
     else
         return liveVersion, nil
@@ -45,4 +51,15 @@ def app_store_version_to_upload_to(projectVersion:)
 
     puts "existing live version = #{liveVersion}, using version #{ourVersion}"
     return ourVersion
+end
+
+def app_store_build_number_with_connect_api()
+    puts "login into spaceship"
+
+    token = Spaceship::ConnectAPI::Token.create
+    Spaceship::ConnectAPI.token = token
+
+    app = Spaceship::ConnectAPI.get_apps(filter: { bundleId: ENV['APP_BUNDLE_IDENTIFIER'] }).first
+
+    return app.get_builds().first.version
 end
