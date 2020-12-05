bd7c43b0a71e18373013a8c1a29c38e91a94aa09
Pierre Slamich <pierre@openfoodfacts.org>
2020-12-05 15:27:40 +0100

    Update test_app.sh

--- scripts/test_app.sh
+++ scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace Calculator.xcworkspace \
-            -scheme Calculator\ iOS \
+xcodebuild -workspace OpenFoodFacts.xcworkspace \
+            -scheme OpenFoodFacts\
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
